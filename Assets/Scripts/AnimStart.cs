8ae17e4922a03602f9b48f794be5cf31afbab97f
ecoknows <66323903+ecoknows@users.noreply.github.com>
2020-10-15 10:58:53 +0800

    Adding Package

---
+++ Assets/Scripts/AnimStart.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimStart : MonoBehaviour
+{
+    AudioSource audio;
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+    /*
+    private void OnEnable()
+    {
+        GetComponent<Animator>().SetTrigger("Start");
+    }*/
+    public void Speak()
+    {
+        audio.Play();
+        //StartCoroutine(waitForSound());
+    }
+    private void OnEnable()
+    {
+        GetComponent<Animator>().SetTrigger("Start");
+    }
+}
